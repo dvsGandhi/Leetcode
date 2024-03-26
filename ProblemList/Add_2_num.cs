55763b4d7094754b605b010da77250e86affab7a
divyesh <divyesh@bbd.co.za>
2024-03-26 13:36:53 +0530

    optimized

--- ProblemList/Add_2_num.cs
+++ ProblemList/Add_2_num.cs
@@ -15,57 +15,84 @@ namespace Leetcode.ProblemList
         }
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            List<int> l1items = GetListItem(l1);
-            List<int> l2items = GetListItem(l2);
-            int listCount = ListCount(l1items, l2items);
-
             int sum = 0;
-            ListNode l3 = new ListNode();
+            int carry = 0;
+            ListNode l3 = head;
 
-            for (int i = 0; i <= listCount - 1 || sum != 0; i++)
+            while (l1 != null || l2 != null)
             {
-                ListNode newNode = new ListNode();
-                if (i < listCount)
+                if (l1 != null)
                 {
-                    if (l1items.Count >= l2items.Count)
-                        if ((i + 1) <= l2items.Count)
-                            sum += (l1items[i] + l2items[i]);
-                        else
-                            sum += l1items[i];
-                    else
-                    {
-                        if ((i + 1) <= l1items.Count)
-                            sum += (l1items[i] + l2items[i]);
-                        else
-                            sum += l2items[i];
-                    }
+                    sum += l1.val;
+                    l1 = l1.next;
                 }
-
-                if (sum.ToString().Count() > 1)
+                if (l2 != null)
                 {
-                    newNode.val = Convert.ToInt32(sum.ToString().Substring(sum.ToString().Count() - 1));
-                    sum = Convert.ToInt32(sum.ToString().Substring(0, sum.ToString().Count() - 1));
+                    sum += l2.val;
+                    l2 = l2.next;
                 }
+                sum += carry;
+                ListNode temp = new ListNode(sum % 10);
+                if(l3 == null)
+                    head = temp;
                 else
-                {
-                    newNode.val = sum;
-                    sum = 0;
-                }
-
-                if (head == null)
-                {
-                    head = newNode;
-                }
-                else
-                {
-                    l3 = head;
-                    while (l3.next != null)
-                    {
-                        l3 = l3.next;
-                    }
-                    l3.next = newNode;
-                }
+                    l3.next = temp;
+                l3 = temp;
+                carry = sum / 10;
+                sum = 0;
             }
+
+            if(carry == 1)
+            {
+                ListNode temp = new ListNode(carry);
+                l3.next = temp;
+                l3 = temp;
+            }
+
+            //for (int i = 0; i <= listCount - 1 || sum != 0; i++)
+            //{
+            //    ListNode newNode = new ListNode();
+            //    if (i < listCount)
+            //    {
+            //        if (l1items.Count >= l2items.Count)
+            //            if ((i + 1) <= l2items.Count)
+            //                sum += (l1items[i] + l2items[i]);
+            //            else
+            //                sum += l1items[i];
+            //        else
+            //        {
+            //            if ((i + 1) <= l1items.Count)
+            //                sum += (l1items[i] + l2items[i]);
+            //            else
+            //                sum += l2items[i];
+            //        }
+            //    }
+
+            //    if (sum.ToString().Count() > 1)
+            //    {
+            //        newNode.val = Convert.ToInt32(sum.ToString().Substring(sum.ToString().Count() - 1));
+            //        sum = Convert.ToInt32(sum.ToString().Substring(0, sum.ToString().Count() - 1));
+            //    }
+            //    else
+            //    {
+            //        newNode.val = sum;
+            //        sum = 0;
+            //    }
+
+            //    if (head == null)
+            //    {
+            //        head = newNode;
+            //    }
+            //    else
+            //    {
+            //        l3 = head;
+            //        while (l3.next != null)
+            //        {
+            //            l3 = l3.next;
+            //        }
+            //        l3.next = newNode;
+            //    }
+            //}
             PrintList();
             Console.ReadLine();
             return head;
