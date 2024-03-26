5b1c9875dbca66d2ff5dcbc1901950d1a3b4dabb
divyesh <divyesh@bbd.co.za>
2024-03-26 13:07:00 +0530

    Add project files.

---
+++ ProblemList/Add_2_num.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.ProblemList
+{
+    internal class Add_2_num
+    {
+        ListNode head;
+        public Add_2_num()
+        {
+            head = null;
+        }
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            List<int> l1items = GetListItem(l1);
+            List<int> l2items = GetListItem(l2);
+            int listCount = ListCount(l1items, l2items);
+
+            int sum = 0;
+            ListNode l3 = new ListNode();
+
+            for (int i = 0; i <= listCount - 1 || sum != 0; i++)
+            {
+                ListNode newNode = new ListNode();
+                if (i < listCount)
+                {
+                    if (l1items.Count >= l2items.Count)
+                        if ((i + 1) <= l2items.Count)
+                            sum += (l1items[i] + l2items[i]);
+                        else
+                            sum += l1items[i];
+                    else
+                    {
+                        if ((i + 1) <= l1items.Count)
+                            sum += (l1items[i] + l2items[i]);
+                        else
+                            sum += l2items[i];
+                    }
+                }
+
+                if (sum.ToString().Count() > 1)
+                {
+                    newNode.val = Convert.ToInt32(sum.ToString().Substring(sum.ToString().Count() - 1));
+                    sum = Convert.ToInt32(sum.ToString().Substring(0, sum.ToString().Count() - 1));
+                }
+                else
+                {
+                    newNode.val = sum;
+                    sum = 0;
+                }
+
+                if (head == null)
+                {
+                    head = newNode;
+                }
+                else
+                {
+                    l3 = head;
+                    while (l3.next != null)
+                    {
+                        l3 = l3.next;
+                    }
+                    l3.next = newNode;
+                }
+            }
+            PrintList();
+            Console.ReadLine();
+            return head;
+        }
+        private List<int> GetListItem(ListNode l)
+        {
+            List<int> items = new List<int>();
+
+            while (l != null)
+            {
+                items.Add(l.val);
+                l = l.next;
+            };
+            return items;
+        }
+
+        private int ListCount(List<int> l1, List<int> l2)
+        {
+            if (l1.Count > l2.Count)
+                return l1.Count;
+            else
+                return l2.Count;
+        }
+
+        private void PrintList()
+        {
+            ListNode temp = new ListNode();
+            temp = this.head;
+            if (temp != null)
+            {
+                while (temp != null)
+                {
+                    Console.Write(temp.val + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine();
+            }
+            else
+                Console.WriteLine("The list is empty");
+
+
+
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leetcode.ProblemList;
+
+namespace Leetcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sum sa = new Sum();
+            //sa.TwoSum(new[] { 2,7,11,15},9);
+
+            Add_2_num addnum = new Add_2_num();
+            ListNode l13 = new ListNode(9, null);
+            ListNode l12 = new ListNode(4, l13);
+            ListNode l11 = new ListNode(2, l12);
+
+            ListNode l24 = new ListNode(9, null);
+            ListNode l23 = new ListNode(4, l24);
+            ListNode l22 = new ListNode(6, l23);
+            ListNode l21 = new ListNode(5, l22);
+
+            addnum.AddTwoNumbers(l11, l21);
+        }
+    }
+    internal class Sum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] output = new int[2];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (((nums[i - 1]) + (nums[i])) == target)
+                {
+                    output[0] = i - 1;
+                    output[1] = i;
+                    Console.Write("[" + output[0] + "," + output[1] + "]");
+                }
+            }
+            return output;
+        }
+    }
+
+}
