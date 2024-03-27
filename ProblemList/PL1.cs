5cc31d1f353e8776997674b63de0cfd9e0fef3a7
divyesh <divyesh@bbd.co.za>
2024-03-27 15:06:05 +0530

    #1

---
+++ ProblemList/PL1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.ProblemList
+{
+    internal class PL1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] output = new int[2];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = 1; j < nums.Length; j++)
+                {
+                    if (((nums[i]) + (nums[j])) == target)
+                    {
+                        output[0] = i;
+                        output[1] = j;
+                        Console.Write("[" + output[0] + "," + output[1] + "]");
+                        return output;
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
