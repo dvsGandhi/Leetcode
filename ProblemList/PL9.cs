2ae682114422975c58bbac4ab3e889e44ccf16f8
divyesh <divyesh@bbd.co.za>
2024-04-02 15:44:23 +0530

    9. Palindrome Number Solved

---
+++ ProblemList/PL9.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.ProblemList
+{
+    internal class PL9
+    {
+        public bool IsPalindrome(int x)
+        {
+            int r, sum = 0;
+            int temp =  x;
+            while (x > 0)
+            {
+                r = x % 10;
+                sum = (sum * 10) + r;
+                x=x/10;
+            }
+            if(temp == sum)
+                return true;
+            else
+                return false;
+        }
+    }
+}
